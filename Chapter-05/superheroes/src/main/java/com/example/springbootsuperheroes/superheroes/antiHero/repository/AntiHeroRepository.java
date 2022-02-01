0d0f8e027ab72466cc412e47a3081ce71ba2b735
Seiji Ralph Villafranca <seijiv13@outlook.com>
2022-02-02 00:28:04 +0800

    Delete Chapter-05 directory

--- Chapter-05/superheroes/src/main/java/com/example/springbootsuperheroes/superheroes/antiHero/repository/AntiHeroRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.springbootsuperheroes.superheroes.antiHero.repository;
-
-import com.example.springbootsuperheroes.superheroes.antiHero.entity.AntiHeroEntity;
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.UUID;
-
-public interface AntiHeroRepository extends CrudRepository<AntiHeroEntity, UUID> {
-}
