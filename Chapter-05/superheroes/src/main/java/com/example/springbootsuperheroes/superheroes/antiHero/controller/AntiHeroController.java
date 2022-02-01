88c78f1fdea4c17580fc84ddfa74781eb46b106a
Seiji Ralph Villafranca <seijiv13@outlook.com>
2022-02-02 00:29:01 +0800

    Added Chapter 5 code

---
+++ Chapter-05/superheroes/src/main/java/com/example/springbootsuperheroes/superheroes/antiHero/controller/AntiHeroController.java
@@ -0,0 +1,79 @@
+package com.example.springbootsuperheroes.superheroes.antiHero.controller;
+
+import com.example.springbootsuperheroes.superheroes.antiHero.dto.AntiHeroDto;
+import com.example.springbootsuperheroes.superheroes.antiHero.entity.AntiHeroEntity;
+import com.example.springbootsuperheroes.superheroes.antiHero.service.AntiHeroService;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("api/v1/anti-heroes")
+public class AntiHeroController {
+    private final AntiHeroService service;
+    private final ModelMapper mapper;
+
+    @GetMapping
+    public List<AntiHeroDto> getAntiHeroes() {
+        // Mapstruct is another dto mapper, but it's not straight forward
+        var antiHeroList = StreamSupport
+                .stream(service.findAllAntiHeroes().spliterator(), false)
+                .collect(Collectors.toList());
+
+
+        return antiHeroList
+                .stream()
+                .map(this::convertToDto)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/{id}")
+    public AntiHeroDto getAntiHeroById(@PathVariable("id") UUID id) {
+        return convertToDto(service.findAntiHeroById(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteAntiHeroById(@PathVariable("id") UUID id) {
+        service.removeAntiHeroById(id);
+    }
+
+    @PostMapping
+    public AntiHeroDto postAntiHero(@Valid @RequestBody AntiHeroDto antiHeroDto) {
+        var entity = convertToEntity(antiHeroDto);
+        var antiHero = service.addAntiHero(entity);
+
+        return convertToDto(antiHero);
+    }
+
+    @PutMapping("/{id}")
+    public void putAntiHero(
+            @PathVariable("id") UUID id,
+            @Valid @RequestBody AntiHeroDto antiHeroDto
+    ) {
+        if (!id.equals(antiHeroDto.getId())) throw new ResponseStatusException(
+                HttpStatus.BAD_REQUEST,
+                "id does not match"
+        );
+
+        var antiHeroEntity = convertToEntity(antiHeroDto);
+        service.updateAntiHero(id, antiHeroEntity);
+    }
+
+    private AntiHeroDto convertToDto(AntiHeroEntity entity) {
+        return mapper.map(entity, AntiHeroDto.class);
+    }
+
+    private AntiHeroEntity convertToEntity(AntiHeroDto dto) {
+        return mapper.map(dto, AntiHeroEntity.class);
+    }
+
+}
