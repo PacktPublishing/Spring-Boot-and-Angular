d6e08a770154ebbb5f4c6fb59f0ac9acfef0bf28
Seiji Ralph Villafranca <seijiv13@outlook.com>
2022-02-02 00:27:04 +0800

    Add files via upload

---
+++ Chapter-05/superheroes/src/main/java/com/example/springbootsuperheroes/superheroes/exception/NotFoundException.java
@@ -0,0 +1,17 @@
+package com.example.springbootsuperheroes.superheroes.exception;
+
+
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+
+    public NotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
---
+++ Chapter-05/superheroes/src/test/java/com/example/springbootsuperheroes/superheroes/SuperheroesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootsuperheroes.superheroes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperheroesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
