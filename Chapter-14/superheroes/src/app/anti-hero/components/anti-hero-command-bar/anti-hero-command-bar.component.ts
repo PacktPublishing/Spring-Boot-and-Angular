1df9addd1d788dd33be8f64770a31092018f2e02
devlinduldulao <webmasterdevlin@gmail.com>
2022-09-25 20:25:10 +0200

    update

--- Chapter-14/superheroes/src/app/anti-hero/components/anti-hero-command-bar/anti-hero-command-bar.component.ts
+++ Chapter-14/superheroes/src/app/anti-hero/components/anti-hero-command-bar/anti-hero-command-bar.component.ts
@@ -1,20 +1,24 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { CommandBarActions } from '../../enums/command-bar-actions.enum';
+import { Component, EventEmitter, OnInit, Output } from "@angular/core";
+import { Router } from "@angular/router";
+import { CommandBarActions } from "../../enums/command-bar-actions.enum";
 
 @Component({
-  selector: 'app-anti-hero-command-bar',
-  templateUrl: './anti-hero-command-bar.component.html',
-  styleUrls: ['./anti-hero-command-bar.component.scss']
+  selector: "app-anti-hero-command-bar",
+  templateUrl: "./anti-hero-command-bar.component.html",
+  styleUrls: ["./anti-hero-command-bar.component.scss"],
 })
 export class AntiHeroCommandBarComponent implements OnInit {
-  @Output() action = new EventEmitter<CommandBarActions>()
-  constructor() { }
+  @Output() action = new EventEmitter<CommandBarActions>();
+  constructor(private router: Router) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   emitAction(action: CommandBarActions) {
     this.action.emit(action);
   }
 
+  logOut() {
+    localStorage.removeItem("token");
+    this.router.navigateByUrl("/login").then();
+  }
 }
