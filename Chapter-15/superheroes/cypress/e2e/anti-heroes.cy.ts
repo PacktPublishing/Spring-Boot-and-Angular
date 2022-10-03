2153b12209cc1359515378020569fd6ee9fbc096
devlinduldulao <webmasterdevlin@gmail.com>
2022-10-03 20:31:44 +0200

    update

--- Chapter-15/superheroes/cypress/e2e/anti-heroes.cy.ts
+++ Chapter-15/superheroes/cypress/e2e/anti-heroes.cy.ts
@@ -26,44 +26,45 @@ describe("Login Page", () => {
     cy.url().should("include", "/login");
     cy.get("[data-cy=auth-title]").should("contain", "Login");
   });
-});
-it("should display logo", () => {
-  cy.get("[data-cy=logo]").should("contain", "Angular CRUD");
-});
 
-it("should render anti-heroes", () => {
-  cy.fixture("anti-heroes").then(function (data) {
-    cy.get("[data-cy=row]").should("have.length", 24);
-  });
-});
-
-it("should remove a card after clicking a delete button", () => {
-  const index = 1;
-  cy.get("[data-cy=delete]").eq(index).click();
-  cy.get("[data-cy=row]").should("have.length", 20);
-});
-
-it("should add a new hero", () => {
-  const firstName = "Bucky";
-  const lastName = "Barnes";
-  const house = "Marvel";
-  const knownAs = "The Winter Soldier";
-
-  cy.get("[data-cy=create]").click();
-  cy.get("[data-cy=firstName]").type(firstName);
-  cy.get("[data-cy=lastName]").type(lastName);
-  cy.get("[data-cy=house]").type(house);
-  cy.get("[data-cy=knownAs]").type(knownAs);
-
-  cy.postCommand("/api/v1/anti-heroes", {
-    firstName,
-    lastName,
-    house,
-    knownAs,
+  it("should display logo", () => {
+    cy.get("[data-cy=logo]").should("contain", "Angular CRUD");
   });
 
-  cy.get("[data-cy=action]").click();
-  cy.fixture("anti-heroes").then(function (data) {
-    cy.get("[data-cy=row]").should("have.length", 24);
+  it("should render anti-heroes", () => {
+    cy.fixture("anti-heroes").then(function (data) {
+      cy.get("[data-cy=row]").should("have.length", 24);
+    });
+  });
+
+  it("should remove a card after clicking a delete button", () => {
+    const index = 1;
+    cy.get("[data-cy=delete]").eq(index).click();
+    cy.get("[data-cy=row]").should("have.length", 20);
+  });
+
+  it("should add a new hero", () => {
+    const firstName = "Bucky";
+    const lastName = "Barnes";
+    const house = "Marvel";
+    const knownAs = "The Winter Soldier";
+
+    cy.get("[data-cy=create]").click();
+    cy.get("[data-cy=firstName]").type(firstName);
+    cy.get("[data-cy=lastName]").type(lastName);
+    cy.get("[data-cy=house]").type(house);
+    cy.get("[data-cy=knownAs]").type(knownAs);
+
+    cy.postCommand("/api/v1/anti-heroes", {
+      firstName,
+      lastName,
+      house,
+      knownAs,
+    });
+
+    cy.get("[data-cy=action]").click();
+    cy.fixture("anti-heroes").then(function (data) {
+      cy.get("[data-cy=row]").should("have.length", 24);
+    });
   });
 });
